72737c64c4c7c80ccacde5cf1a4107f09108f076
AlbertoSSC <albertossc@gmail.com>
2024-02-09 12:04:58 +0100

    part01: exercise 1.12, anecdotes step1

--- part01/src/App.jsx
+++ part01/src/App.jsx
@@ -1,48 +1,29 @@
 import { useState } from "react";
-import { Header } from "./Components/Header";
-import { ButtonComp } from "./Components/Button";
-import { StatisticsLine } from "./Components/StatisticsLine";
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  const title = "Give feecbacks";
-  const total = good + neutral + bad;
-  const average = (good - bad) / total;
-  const positive = (good / total) * 100;
+  const [selected, setSelected] = useState(0);
+
+  const chooseRandom = () => {
+    const random = Math.floor(Math.random() * anecdotes.length);
+    setSelected(random);
+  };
 
   return (
-    <>
-      <Header title={title} />
-
-      <ButtonComp handleClick={() => setGood(good + 1)} text="good" />
-      <ButtonComp handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <ButtonComp handleClick={() => setBad(bad + 1)} text="bad" />
-
-      <h2>StatisticsLine</h2>
-
-      {good === 0 && bad === 0 && neutral === 0 ? (
-        <span>No feecbacks given </span>
-      ) : (
-        <table>
-          <colgroup>
-            <col style={{ backgroundColor: "lightgrey" }} />
-            <col />
-          </colgroup>
-          <tbody>
-            <StatisticsLine text="good" value={good} />
-            <StatisticsLine text="neutral" value={neutral} />
-            <StatisticsLine text="bad" value={bad} />
-            <StatisticsLine text="total" value={total} />
-            <StatisticsLine text="average" value={average.toFixed(2)} />
-            <StatisticsLine text="positive" value={positive.toFixed(2)} />
-          </tbody>
-        </table>
-      )}
-    </>
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={chooseRandom}>Next anecdote</button>
+    </div>
   );
 };
 
