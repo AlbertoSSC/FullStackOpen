172afba01a048ae6d20725cd1f0865b1b75b84c4
AlbertoSSC <albertossc@gmail.com>
2024-02-13 12:56:38 +0100

    part02/ 2.4: Course information step9

--- part02/src/App.jsx
+++ part02/src/App.jsx
@@ -1,33 +1,51 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  };
+  const course = [
+    {
+      id: 1,
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   const Course = ({ course }) => {
-    const Header = ({ name }) => <h1>{name}Hola</h1>;
+    const Header = ({ name }) => <h2>{name}</h2>;
 
     const Total = ({ parts }) => (
       <p>
@@ -54,14 +72,21 @@ const App = () => {
 
     return (
       <div>
-        <Header course={course.name} />
+        <Header name={course.name} />
         <Content parts={course.parts} />
         <Total parts={course.parts} />
       </div>
     );
   };
 
-  return <Course course={course} />;
+  return (
+    <>
+      <h1>Web development curriculum</h1>
+      {course.map((c) => (
+        <Course key={c.id} course={c} />
+      ))}
+    </>
+  );
 };
 
 export default App;
