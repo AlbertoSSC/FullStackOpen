df3cd3bb3685971e0cf720768d91f86b0dd5a2a1
AlbertoSSC <albertossc@gmail.com>
2024-02-13 13:00:55 +0100

    part02/ 2.5: Course information step10

--- part02/src/App.jsx
+++ part02/src/App.jsx
@@ -1,3 +1,5 @@
+import { Course } from "./course.component";
+
 const App = () => {
   const course = [
     {
@@ -43,41 +45,6 @@ const App = () => {
       ],
     },
   ];
-
-  const Course = ({ course }) => {
-    const Header = ({ name }) => <h2>{name}</h2>;
-
-    const Total = ({ parts }) => (
-      <p>
-        <strong>
-          Total of {parts.reduce((sum, part) => sum + part.exercises, 0)}
-          exercises
-        </strong>
-      </p>
-    );
-
-    const Part = ({ part }) => (
-      <p>
-        {part.name} {part.exercises}
-      </p>
-    );
-
-    const Content = ({ parts }) => (
-      <>
-        {parts.map((part) => (
-          <Part key={part.id} part={part} />
-        ))}
-      </>
-    );
-
-    return (
-      <div>
-        <Header name={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-      </div>
-    );
-  };
 
   return (
     <>
